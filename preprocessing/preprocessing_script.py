bb429685390b5b449073534a8ca15c83837b25a8
Attornado <73835871+attornado@users.noreply.github.com>
2022-08-23 00:33:14 +0200

    Completed preprocessing script, refactored/added some constants and fixed some bugs in preprocessing functions.
    Signed-off-by: Attornado <73835871+Attornado@users.noreply.github.com>

--- preprocessing/preprocessing_script.py
+++ preprocessing/preprocessing_script.py
@@ -1,90 +1,111 @@
 import os
-from bidict import bidict
+import random
+from typing import final
+from matplotlib import pyplot as plt
 import numpy as np
-import cv2
-from tqdm import tqdm
-from preprocessing.constants import IMG_HEIGHT, IMG_WIDTH, TRAIN_PATH, TEST_PATH, CHANNELS, TEST_PATH_IMAGES, \
-    TRAIN_PATH_IMAGES_LABELS, VALIDATION_PATH_IMAGES_LABELS, VALIDATION_SIZE, RANDOM_STATE
-from preprocessing.utils import get_class_names
+import pandas as pd
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from preprocessing.utils import get_feature_and_label, drop_image_id, train_preprocess, decode_label, decode_image, \
+    get_feature
+from preprocessing.constants import VALIDATION_SIZE, TRAIN_CSV, TEST_CSV, RANDOM_STATE, EVAL_SIZE, AUTOTUNE, \
+    AUGMENTATION_RATIO, BUFFER_SIZE, TRAIN_PATH_CLEANED, EVALUATION_PATH_CLEANED, VALIDATION_PATH_CLEANED, \
+    TEST_PATH_CLEANED
 
 
-def _create_training_data(path: str, classes: bidict[int, str]) -> (list[str], np.ndarray):
-    """
-    Creates the training data arrays.
-
-    :param path: training dataset path.
-    :param classes: class bidirectional dictionary.
-    :return: a list containing the filenames and a numpy array containing the corresponding labels.
-    """
-    images = []
-    labels = []
-
-    for class_num in classes:
-
-        # Get the full class folder path
-        class_path = os.path.join(path, classes[class_num])
-
-        desc = f"Creating training data (images+labels) for class '{classes[class_num]}' ({class_num})"
-        # For each image
-        for img in tqdm(os.listdir(class_path), desc=desc):
-
-            images.append(os.path.join(class_path, img))
-
-            # Insert the image label into the corresponding list
-            labels.append(class_num)
-
-    # Convert lists into numpy arrays
-    labels = np.array(labels, dtype=int)
-
-    return images, labels
+# Set random state
+os.environ['PYTHONHASHSEED'] = str(RANDOM_STATE)
+random.seed = RANDOM_STATE
+np.random.seed = RANDOM_STATE
+tf.random.set_seed(RANDOM_STATE)
 
 
-def _create_test_data(path: str) -> np.ndarray:
-    """
-    Creates the test data arrays.
-
-    :param path: test dataset path.
-    :return: a numpy array with shape (dataset_size, IMG_HEIGHT, IMG_WIDTH, channels) representing the images of the
-        test set.
-    """
-    images = []
-
-    # For each image
-    for img in tqdm(os.listdir(path), desc="Extracting test data (images)"):
-
-        # Read, resize and append the image to the training set lists
-        img_array = cv2.imread(os.path.join(path, img))
-        new_array = cv2.resize(img_array, (IMG_WIDTH, IMG_HEIGHT))
-        images.append(new_array)
-
-    # Convert lists into numpy arrays
-    images = np.reshape(np.array(images), (len(images), IMG_HEIGHT, IMG_WIDTH, CHANNELS))
-
-    return images
+# Preprocessing script constants
+_BATCH_SIZE: final = 32
 
 
 def main():
-    # Get class names
-    class_names = get_class_names(TRAIN_PATH)
 
-    # Get images and labels from dataset
-    images, labels = _create_training_data(TRAIN_PATH, class_names)
-    test_images = _create_test_data(TEST_PATH)
+    # Read csv files
+    presplit_csv_data = pd.read_csv(TRAIN_CSV)
+    test_csv_data = pd.read_csv(TEST_CSV)
 
-    # Split in train/validation
-    train_images, val_images, train_labels, val_labels = train_test_split(
-        images,
-        labels,
+    # Train/eval split (think about eval as labeled test set)
+    train_csv_data, eval_csv_data = train_test_split(
+        presplit_csv_data,
+        test_size=EVAL_SIZE,
+        shuffle=True,
+        random_state=RANDOM_STATE
+    )
+
+    # Train/validation split
+    train_csv_data, val_csv_data = train_test_split(
+        train_csv_data,
         test_size=VALIDATION_SIZE,
         shuffle=True,
         random_state=RANDOM_STATE
     )
 
-    # Store train/test arrays and labels to npz file
-    np.savez(file=TRAIN_PATH_IMAGES_LABELS, images=train_images, labels=train_labels)
-    np.savez(file=VALIDATION_PATH_IMAGES_LABELS, images=val_images, labels=val_labels)
-    np.save(file=TEST_PATH_IMAGES, arr=test_images)
+    # Get train filenames and labels
+    train_images = pd.DataFrame(train_csv_data[['image']].values.tolist())
+    train_labels = pd.DataFrame(train_csv_data[['name']].values.tolist())
+
+    # Get evaluation filenames and labels (think about this as the labeled test set)
+    eval_images = pd.DataFrame(eval_csv_data[['image']].values.tolist())
+    eval_labels = pd.DataFrame(eval_csv_data[['name']].values.tolist())
+
+    # Get unlabeled test filenames
+    test_images = pd.DataFrame(test_csv_data[['image']].values.tolist())
+
+    # Get validation filenames and labels
+    val_images = pd.DataFrame(val_csv_data[['image']].values.tolist())
+    val_labels = pd.DataFrame(val_csv_data[['name']].values.tolist())
+
+    # Convert train set to tensorflow format
+    train_ds_prebatch = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+    train_ds_prebatch = train_ds_prebatch.map(get_feature_and_label, num_parallel_calls=AUTOTUNE)
+
+    # Convert evaluation set into tensorflow format
+    eval_ds_prebatch = tf.data.Dataset.from_tensor_slices((eval_images, eval_labels))
+    eval_ds_prebatch = eval_ds_prebatch.map(get_feature_and_label, num_parallel_calls=AUTOTUNE)
+
+    # Convert validation set into tensorflow format
+    val_ds_prebatch = tf.data.Dataset.from_tensor_slices((val_images, val_labels))
+    val_ds_prebatch = val_ds_prebatch.map(get_feature_and_label, num_parallel_calls=AUTOTUNE)
+
+    # Convert test set to tensorflow format
+    test_ds_prebatch = tf.data.Dataset.from_tensor_slices((test_images))
+    test_ds_prebatch = test_ds_prebatch.map(get_feature, num_parallel_calls=AUTOTUNE)
+
+    # At this point our datasets have an image, label, and image_id component, so drop image id
+    train_ds_prebatch = train_ds_prebatch.map(drop_image_id)
+    val_ds_prebatch = val_ds_prebatch.map(drop_image_id)
+    # Maybe drop from eval and test sets too?
+
+    # Finish preprocessing of the training set, performing data augmentation
+    train_ds_prebatch = train_ds_prebatch.repeat(AUGMENTATION_RATIO)
+    train_ds_prebatch = train_ds_prebatch.map(train_preprocess, num_parallel_calls=AUTOTUNE)
+    train_ds_prebatch = train_ds_prebatch.shuffle(buffer_size=BUFFER_SIZE, reshuffle_each_iteration=True)
+
+    # Get a batch from the train set to test things up, plotting shapes and the one image
+    x, y = next(iter(train_ds_prebatch.batch(_BATCH_SIZE)))
+    print("Shape input batch: ", x.shape)
+    print("Shapes:")
+    print(train_ds_prebatch)
+    image, label = next(iter(train_ds_prebatch.skip(2)))
+    print("image shape:", image.shape)
+    print("label shape:", label.shape, "\t\t\t", label)
+    print()
+    print(decode_label(label))
+    plt.imshow(decode_image(image))  # for normal run display image
+    plt.show()
+    # IPython.display.display(decode_image(image))  # for Jupyter/Colab/Kaggle display image
+
+    # Store train/eval/validation pre-batch datasets with tensorflow
+    tf.data.experimental.save(dataset=train_ds_prebatch, path=TRAIN_PATH_CLEANED)
+    tf.data.experimental.save(dataset=eval_ds_prebatch, path=EVALUATION_PATH_CLEANED)
+    tf.data.experimental.save(dataset=val_ds_prebatch, path=VALIDATION_PATH_CLEANED)
+    tf.data.experimental.save(dataset=test_ds_prebatch, path=TEST_PATH_CLEANED)
 
 
 if __name__ == "__main__":
